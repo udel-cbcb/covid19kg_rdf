413284990f83c95341c83545b7a3ab6f8aa88457
Chuming Chen <chenc@dbi.udel.edu>
2022-04-25 17:35:52 -0400

    Update litcovid2BioC_json_to_ttl.sh

--- litcovid/litcovid2BioC_json_to_ttl.sh
+++ litcovid/litcovid2BioC_json_to_ttl.sh
@@ -7,8 +7,8 @@ do
   echo $f
   file="$(basename $f)"
   file="${file%%.*}"
-  cat $f | java -jar ../json2rdf-1.0.1-jar-with-dependencies.jar $target_uri | ../apache-jena-4.1.0/bin/riot  --formatted=TURTLE > rdf/metadata/$file.ttl
-  echo "rdf/metadata/$file.ttl"
+  cat $f | java -jar ../json2rdf-1.0.1-jar-with-dependencies.jar $target_uri | ../apache-jena-4.1.0/bin/riot  --formatted=TURTLE > rdf/litcovid/$file.ttl
+  echo "rdf/litcovid/$file.ttl"
 done
 
 
